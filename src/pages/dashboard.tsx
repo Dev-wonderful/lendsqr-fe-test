ad268e80613982a82c1f6ac947d9b780436e2941
Dev-wonderful <adeonederful20@gmail.com>
2023-06-08 15:21:17 +0100

    dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
